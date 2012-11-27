a145e53e6672798ff840b0e943c35db23cab0746
Rui Shen <shenrui01@gmail.com>
2012-11-28 05:13:30 +0800

    添加ralink平台 rt305x cpuinfo显示正确的cpuclock

--- target/linux/ramips/files/arch/mips/ralink/rt305x/setup.c
+++ target/linux/ramips/files/arch/mips/ralink/rt305x/setup.c
@@ -25,6 +25,8 @@
 #include <asm/mach-ralink/rt305x_regs.h>
 #include "common.h"
 
+static unsigned int ramips_cpu_freq;
+
 static void rt305x_restart(char *command)
 {
 	rt305x_sysc_wr(RT305X_RESET_SYSTEM, SYSC_REG_RESET_CTRL);
@@ -52,12 +54,14 @@ void __init ramips_soc_setup(void)
 	rt305x_sysc_base = ioremap_nocache(RT305X_SYSC_BASE, PAGE_SIZE);
 	rt305x_memc_base = ioremap_nocache(RT305X_MEMC_BASE, PAGE_SIZE);
 
+	rt305x_detect_sys_type();
 	rt305x_clocks_init();
 
 	clk = clk_get(NULL, "cpu");
 	if (IS_ERR(clk))
 		panic("unable to get CPU clock, err=%ld", PTR_ERR(clk));
 
+	ramips_cpu_freq = clk_get_rate(clk);
 	printk(KERN_INFO "%s running at %lu.%02lu MHz\n", ramips_sys_type,
 		clk_get_rate(clk) / 1000000,
 		(clk_get_rate(clk) % 1000000) * 100 / 1000000);
@@ -85,4 +89,9 @@ void __init plat_time_init(void)
 		panic("unable to get CPU clock, err=%ld", PTR_ERR(clk));
 
 	mips_hpt_frequency = clk_get_rate(clk) / 2;
+}
+
+unsigned int ramips_get_cpu_freq(void)
+{
+	return ramips_cpu_freq;
 }
